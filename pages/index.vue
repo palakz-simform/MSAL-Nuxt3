b44191ca05d6ebf1fe30c72a6a76e5152485250c
Palak Zalavadia <palak.z@simformsolutions.com>
2024-02-15 10:37:28 +0530

    feat: UI of login and index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,47 @@
 <template>
-    <h1>Home Page</h1>
+<div class="container">
+    <div class="profile">
+        <img src="../assets/images/profile.jpeg" height="250"/>
+    </div>
+    <div class="name">
+        <h1>Welcome, Palak Zalavadia!</h1>
+    </div>
+    <div class="username">
+        <h4>palak.z@simformsolutions.com</h4>
+    </div>
+      <div class="button">
+            <button class="btn">Logout</button>
+    </div>
+</div>
 </template>
+<style scoped>
+.container{
+    display: flex;
+    height: 80vh;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center ;
+}
+img{
+    border-radius: 50%;
+    border: 6px solid rgb(249, 186, 186);
+    padding: 6px;
+}
+
+h1{
+    margin-bottom: 0px;
+    font-weight: normal;
+}
+.btn{
+    padding: 15px 40px 15px 40px;
+    font-size:medium;
+    background-color: #ff5a1f;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    color: white;
+    font-weight: bold;
+    font-family: "Roboto", sans-serif;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="image">
+            <img src="../assets/images/microsoft.png"   height="180"/>
+        </div>
+        <div class="text">
+           <h1>MSAL Authentication in Nuxt3 </h1>
+        </div>
+        <div class="button">
+            <button class="btn">Login with Microsoft 365</button>
+        </div>
+    </div>
+</template>
+<style scoped>
+.container{
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn{
+    padding: 15px 40px 15px 40px;
+    font-size:medium;
+    background-color: #ff5a1f;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    color: white;
+    font-weight: bold;
+    font-family: "Roboto", sans-serif;
+}
+</style>
